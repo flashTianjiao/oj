0f80cb98978d68d4462eb57391326f295b0e96a0
dongtianjiao <flashtianjiao@163.com>
2019-09-16 23:14:25 +0800

    leetcode repository

---
+++ leetcode/onebit_and_twobit.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 22:25:25 2019
+
+@author: Administrator
+"""
+
+
+    
+
+def isOneBit(bits):
+    i = 0
+    while i < len(bits) - 1:
+        if bits[i] == 1:
+            i += 2
+        else:
+            i += 1
+    if i == len(bits) - 1:
+        return True
+    else:
+        return False
+
+
+while True:
+    bits = input()
+    print isOneBit(bits)
